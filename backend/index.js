354cea5f73cf630d5a07418f50115006645bf559
Faizansatti14 <sattifaizan053@gmail.com>
2026-03-02 12:37:30 +0500

    removing api key

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3001;
 
 // Initialize Resend with your API key
-const resend = new Resend('re_7iKibE2d_HAX9agvwPnnpLxHKXsjzHouV'); 
+const resend = new Resend(''); 
 
 // Middleware
 app.use(cors());
